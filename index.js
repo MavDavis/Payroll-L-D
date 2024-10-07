87918cbac498833234b149d99eeeea1a0ee57e36
Mavdavis <davidmarvelous@seamlesshr.com>
2024-10-07 19:35:37 +0100

    ts completion

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ var list = [{ id: "hy " }];
 var user = { id: 1, name: "me" };
 var cid = "x";
 var ctid = cid;
+var addNum = function (x, y) {
+    return x + y;
+};
+// if you pass in a string to the params while calling the func, it will fail.
+var addedNum = addNum(3, 3);
 //x can be changed to anything 
 // tsc --watch index to run file in watch mode
--- index.ts
+++ index.ts
@@ -6,6 +6,10 @@ let list: object[] =[{id:"hy "}]
 const user:User ={id:1,name:"me"}
 let cid:any = "x"
 let ctid =<number>cid
-
+const addNum = (x:number, y:number):number=>{
+return x + y 
+}
+// if you pass in a string to the params while calling the func, it will fail.
+const addedNum : number = addNum(3,3)
 //x can be changed to anything 
 // tsc --watch index to run file in watch mode
